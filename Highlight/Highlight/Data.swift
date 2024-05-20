70a60e8a38a90e0dc808c26e4b265490b2d9f2b6
Avery Zheng <averyzheng2020@gmail.com>
2024-05-20 13:06:25 -0400

    Created User

--- Highlight/Highlight/Data.swift
+++ Highlight/Highlight/Data.swift
@@ -32,14 +32,6 @@ Users:
 
 import Foundation
 
-class User {
-    var Relation: [User]
-    
-    init(Relation: [User]) {
-        self.Relation = Relation
-    }
-}
-
 
 
 
---
+++ Highlight/Highlight/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Highlight
+//
+
+import Foundation
+
+class User {
+    var Relation: [User]
+    
+    init(Relation: [User]) {
+        self.Relation = Relation
+    }
+}
