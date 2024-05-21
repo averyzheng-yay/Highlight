f7b1cb44f9967cad08d7d870323d0dd2b0b1ff39
clazzy <jianingchen16@gmail.com>
2024-05-21 10:06:56 -0400

    inter font

--- Highlight/Highlight/ContentView.swift
+++ Highlight/Highlight/ContentView.swift
@@ -8,17 +8,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Highlight")
+                .customFont(.medium, 30)
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
