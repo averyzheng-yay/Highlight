3c301948726e1b078ea52800d7c400d1ec503e9f
Avery Zheng <averyzheng2020@gmail.com>
2024-05-27 19:55:51 -0400

    ViewModel.swift comments

--- Highlight/Highlight/ViewModel.swift
+++ Highlight/Highlight/ViewModel.swift
@@ -2,8 +2,9 @@
 
 import SwiftUI
 
+// Back end class
 class PhotoJournalViewModel: ObservableObject {
-    @Published var entries: [Entries] = []
+    @Published var entries: [Entries] = [] // Array that stores all entries
     private let locationManager = LocationManager()
     
     func addEntry(title: String, image: UIImage, text: String) {
@@ -13,15 +14,17 @@ class PhotoJournalViewModel: ObservableObject {
             let newEntry = PhotoEntries(title: title, image: image, text: text, date: Date(), location: location)
             self.entries.insert(newEntry, at:0)
             self.locationManager.stopUpdatingLocation()
+            // Makes sure to only record the location when adding the entry
         }
     }
     
     func removeEntry(entry: Entries) {
         if let index = entries.firstIndex(where: { $0.id == entry.id }) {
-            entries.remove(at: index)
+            entries.remove(at: index) // only removes the entry that matches the id of the entry given
         }
     }
     
+    // Variable used to create the collage later
     var allPhotos: [UIImage] {
         entries.compactMap { $0 as? PhotoEntries }.map { $0.image }
     }
